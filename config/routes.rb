992f763ee843c8d304fc1ad5d1b022f7d0827a4a
mnemoto83 <nemoccho1031@gmail.com>
2023-06-25 00:45:25 +0000

    2nd No6

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   # PostsController
   get 'posts/new', to: 'posts#new', as: 'new_post'
   
-  get 'topics/new ', to: 'topics#new', as: 'new_topic'
+  get 'topics/new', to: 'topics#new', as: 'new_topic'
   get 'topics/edit', to: 'topics#edit', as: 'edit_topic'
 end
